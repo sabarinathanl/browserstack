390b9799c30bdad343e354863d5eda81c2e8ce73
Sabarinathan L <sabarisabari8682@gmail.com>
2026-08-12 11:08:48 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ const config: PlaywrightTestConfig = {
   reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 10000,
     ignoreHTTPSErrors: true,
